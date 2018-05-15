d473055cf0f63765f9128c7017272b20401c9a9d
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-15 18:13:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 katz_deli = []
 
 def line(katz_deli)
-  SIZED = katz_deli.size()
-  if SIZED === 0
+  sized = katz_deli.size()
+  if sized === 0
     puts "The line is currently empty."
   else
     lined = "The line is currently:"
 
     step = 0
-    while step < SIZED
+    while step < sized
       lined << "#{step}. #{katz_deli[step]}"
     end
 
